33fe2171948859c778bc316abf42208fcb4462ec
Michael Muscat <michael.muscat@outlook.com>
2022-10-07 12:39:53 +1100

    refactor: cache observable selectors

--- projects/core/src/select.ts
+++ projects/core/src/select.ts
@@ -1,6 +1,7 @@
 import {inject, KeyValueDiffers, ProviderToken} from "@angular/core";
-import {defer, distinctUntilChanged, filter, map, Observable, startWith} from "rxjs";
+import {defer, distinctUntilChanged, filter, map, Observable, share, startWith} from "rxjs";
 import {FLUSHED} from "./providers";
+import {getMeta, selector, setMeta} from "./metadata";
 
 export type Selector<T> = {
    [key in keyof T]: Observable<T[key]>
@@ -12,23 +13,39 @@ export function select<T extends {}>(token: ProviderToken<T>): Selector<T> {
 
    return new Proxy(store, {
       get(target: T, property: PropertyKey) {
-         return defer(() => flushed.pipe(
-            filter((context): context is T => context === store),
-            map(() => store[property as keyof T]),
-            startWith(store[property as keyof T]),
-            distinctUntilChanged(Object.is)
-         ))
+         const cache = getMeta(selector, store, property)
+         if (cache) {
+            return cache
+         } else {
+            const source = defer(() => flushed.pipe(
+               filter((context): context is T => context === store),
+               map(() => store[property as keyof T]),
+               startWith(store[property as keyof T]),
+               distinctUntilChanged(Object.is),
+               share()
+            ))
+            setMeta(selector, source, store, property)
+            return source
+         }
       }
    }) as unknown as Selector<T>
 }
 
 export function selectStore<T extends {}>(token: ProviderToken<T>): Observable<T> {
    const store = inject(token)
-   const differ = inject(KeyValueDiffers).find(store).create()
-   differ.diff(store)
-   return inject(FLUSHED).pipe(
-      filter((context): context is T => context === store),
-      startWith(store),
-      distinctUntilChanged((value) => differ.diff(value) === null)
-   )
+   const cache = getMeta(selector, store) as Observable<T>
+   if (cache) {
+      return cache
+   } else {
+      const differ = inject(KeyValueDiffers).find(store).create()
+      differ.diff(store)
+      const source = inject(FLUSHED).pipe(
+         filter((context): context is T => context === store),
+         startWith(store),
+         distinctUntilChanged((value) => differ.diff(value) === null),
+         share()
+      )
+      setMeta(selector, source, store)
+      return source
+   }
 }
